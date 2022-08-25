19c2d6416b98bec25ac57256ea6893b966865878
samluiz <samuelluizrl@outlook.com>
2022-08-25 10:48:11 -0300

    fix broken url

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from .views import taskApi
 
 urlpatterns = [
-  path(r'^tasks/$', taskApi),
+  re_path(r'^tasks/$', taskApi),
   re_path(r'^tasks/([0-9]+)$', taskApi)
 ]
